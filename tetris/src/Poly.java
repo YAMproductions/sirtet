e1d057740d68695d5df253e00d07ea3ced04d6a8
ZiadS <ziadgiliana@gmail.com>
2014-05-13 14:29:07 +0200

    saaja

---
+++ tetris/src/Poly.java
@@ -0,0 +1,43 @@
+
+public class Poly {
+
+	private SquareType[][] poly;
+	
+	/**
+	 * A general class to make all type of blocks
+	 * @param br
+	 * @param width
+	 * @param height
+	 */
+	
+	public Poly(SquareType[][] br, int width, int height){
+		this.poly = SquareType[][]
+				
+	}
+	/**
+	 * returns a blocktype
+	 * @return
+	 */
+	
+	public SquareType[][] getPoly(){
+		return poly;
+		
+	}
+	/**
+	 * returns the length of the block
+	 * @return
+	 */
+	public int getPolyLength(){
+		return;
+	}
+	
+	/**
+	 * Rotates the block 
+	 */
+	public void rotateRight(){
+		
+		
+	}
+	
+	
+}
