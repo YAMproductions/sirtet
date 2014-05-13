c1a6cd1242e20c13f06ad7ce5103c3763b77122a
ZiadS <ziadgiliana@gmail.com>
2014-05-13 14:18:33 +0200

    Lite gejer

---
+++ src/Poly.java
@@ -0,0 +1,40 @@
+
+public class Poly {
+	
+	/**
+	 * A general class to make all type of blocks
+	 * @param br
+	 * @param width
+	 * @param height
+	 */
+	
+	public Poly(SquareType[][] br, int width, int height){
+
+				
+	}
+	/**
+	 * returns a blocktype
+	 * @return
+	 */
+	
+	public SquareType[][] getPoly(){
+		
+	}
+	/**
+	 * returns the length of the block
+	 * @return
+	 */
+	public int getPolyLength(){
+		return 1;
+	}
+	
+	/**
+	 * Rotates the block 
+	 */
+	public void rotateRight(){
+		//Rotera 
+		
+	}
+	
+	
+}
